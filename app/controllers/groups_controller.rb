767195ba0a13b1550bbe081343aec4a2393a28f1
achalbi <achal.rvce@gmail.com>
2013-01-24 23:17:22 +0530

    create-group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,22 @@
 class GroupsController < ApplicationController
+before_filter :signed_in_user, only: [:create, :destroy]
+	
   def new
+  	@group = Group.new
   end
   def index
+
   	@groups = Group.paginate(page: params[:page], :per_page => 10)
   end
+
+  def create
+    @group = current_user.groups.build(params[:group])
+    if @group.save
+      flash[:success] = "Group created!"
+      redirect_to :action => :index
+    else
+        redirect_to :action => :index
+    end 
+
+    end
 end
