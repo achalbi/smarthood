b840df52292fb5d99d60d7050f6ecebd8680d50c
achalbi <achal.rvce@gmail.com>
2013-02-01 02:28:28 +0530

    rm ug_col_mig

--- db/migrate/20130131173347_remove_user_group_id_from_groupposts.rb
+++
@@ -1,9 +0,0 @@
-class RemoveUserGroupIdFromGroupposts < ActiveRecord::Migration
-  def up
-    remove_column :groupposts, :user_group_id
-  end
-
-  def down
-    add_column :groupposts, :user_group_id, :string
-  end
-end
