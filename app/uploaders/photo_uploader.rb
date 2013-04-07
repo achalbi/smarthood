22ad9995c8cbf139c523e2718f0f56f429889955
achalbi <achal.rvce@gmail.com>
2013-04-07 20:33:00 +0530

    photo-edit

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -40,7 +40,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
    version :thumb do
-     process :resize_to_fit => [200, 200]
+     process :resize_to_fill => [200, 200]
    end 
    version :medium do
      process :resize_to_fit => [370, 370]
