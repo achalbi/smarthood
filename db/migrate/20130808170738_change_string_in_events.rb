c9949cf61c4dffc0b9e767a38767870549869c0d
achalbi <achal.rvce@gmail.com>
2013-08-11 21:17:42 +0530

    event show inprg

--- db/migrate/20130808170738_change_string_in_events.rb
+++ db/migrate/20130808170738_change_string_in_events.rb
@@ -1,6 +1,6 @@
 class ChangeStringInEvents < ActiveRecord::Migration
 	def change
-		remove_index "events", ["creator", "created_at", "starts_at", "ends_at"]
+		#remove_index "events", ["creator", "created_at", "starts_at", "ends_at"]
     	change_column :events, :privacy, :string
 		add_index "events", ["creator", "created_at", "starts_at", "ends_at"], :name => "inx_evnts"
   	end
