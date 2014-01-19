c5e17ea0c8adf4e8bb8f252a29f42299ceceedea
achalbi <achal.rvce@gmail.com>
2014-01-20 00:55:56 +0530

    buysell script edit

--- db/migrate/20131211030442_remove_userinfoid_to_user.rb
+++
@@ -1,9 +0,0 @@
-class RemoveUserinfoidToUser < ActiveRecord::Migration
-  def up
-    remove_column :users, :user_info_id
-  end
-
-  def down
-    add_column :users, :user_info_id, :integer
-  end
-end
