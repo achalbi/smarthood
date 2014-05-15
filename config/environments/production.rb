cdcb92a8b066be633389c87ad46dcadc369ff644
achalbi <achal.rvce@gmail.com>
2014-05-15 22:33:34 +0530

    prod mailer host set

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,11 @@ EMS::Application.configure do
   # and use secure cookies.
   #config.force_ssl = true
 
+  config.action_mailer.default_url_options = { :host => "smarthood.in" }
+
+
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
